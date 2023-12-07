a74787c6f363ad70047934157036c86bbe98d532
Egemen Unsur <sabitozbilgic2306@gmail.com>
2023-12-07 15:53:34 +0300

    initial dockerized

---
+++ UI/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Yildiz_Okullari.Models
+{
+	public class ErrorViewModel
+	{
+		public string? RequestId { get; set; }
+
+		public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+	}
+}
