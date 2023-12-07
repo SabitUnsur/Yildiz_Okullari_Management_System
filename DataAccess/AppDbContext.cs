306327969ede9902a8408a879edce90e2ed042a9
Egemen Unsur <sabitozbilgic2306@gmail.com>
2023-12-07 22:18:17 +0300

    Connection string and configs handled, local migration initiated.

--- DataAccess/AppDbContext.cs
+++ DataAccess/AppDbContext.cs
@@ -1,6 +1,7 @@
 ﻿using Entities;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -11,9 +12,26 @@ namespace DataAccess
 {
     public class AppDbContext:IdentityDbContext<Person,AppRole,string>
     {
+        public AppDbContext() : base()
+        {
+            
+        }
         public AppDbContext(DbContextOptions<AppDbContext> options):base(options)
         {
         }
-        public DbSet<Person> Persons { get; set;}
+
+		protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+		{
+			if (!optionsBuilder.IsConfigured)
+			{
+				IConfigurationRoot configuration = new ConfigurationBuilder()
+				   .SetBasePath(Directory.GetCurrentDirectory())
+				   .AddJsonFile("appsettings.json")
+				   .Build();
+				var connectionString = configuration.GetConnectionString("SqlConnection");
+				optionsBuilder.UseNpgsql(connectionString);
+			}
+		}
+		public DbSet<Person> Persons { get; set;}
     }
 }
